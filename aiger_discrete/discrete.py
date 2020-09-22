ff6326abc329739335c107aa212c2fe097ebc7df
Marcell Vazquez-Chanlatte <mvc@linux.com>
2020-09-22 11:41:10 -0700

    Implemented parallel composition.

--- aiger_discrete/discrete.py
+++ aiger_discrete/discrete.py
@@ -1,11 +1,16 @@
 from __future__ import annotations
 
 from collections import defaultdict
-from typing import Any, Callable, Mapping, Sequence
+from typing import Any, Callable, Mapping, Optional, Union
+from uuid import uuid1
 
 import aiger_bv as BV
 import attr
 import funcy as fn
+
+
+def fresh():
+    return str(uuid1())
 
 
 # TODO: implement py-aiger API
@@ -33,7 +38,24 @@ class DiscreteCirc:
         if self.valid_id not in self.circ.outputs:
             raise ValueError(f"Missing validation output: {self.valid_id}.")
         elif self.circ.omap[self.valid_id].size != 1:
-            raise Valid_Id(f"Validation output must be size 1.")
+            raise ValueError("Validation output must be size 1.")
+
+    def rename_valid(self, name: Optional[str] = None) -> DiscreteCirc:
+        if name is None:
+            name = fresh()
+        if name in self.outputs:
+            raise ValueError(f"{name} conflicts with current outputs.")
+        circ = self.circ['o', {self.valid_id: name}]
+        return attr.evolve(self, circ=circ, valid_id=name)
+
+    def assume(self, pred: BV.AIGBV) -> DiscreteCirc:
+        assert len(pred.outputs) == 1
+        func = from_aigbv(pred.outputs, valid_id=fn.first(pred.outputs))
+        return self | func
+
+    @property
+    def _vexpr(self):
+        return BV.uatom(1, self.valid_id)
 
     @property
     def inputs(self): return self.circ.inputs
@@ -57,14 +79,24 @@ class DiscreteCirc:
         if not valid:
             raise ValueError(f"Invalid inputs: {inputs}")
         del omap[self.valid_id]
-        
+
         omap = {
             k: self.output_encodings[k].decode(v) for k, v in omap.items()
         }
         return omap, lmap
 
-    def __or__(self, other: DiscreteCirc) -> DiscreteCirc:
-        pass
+    def __or__(self, other: Union[DiscreteCirc, BV.AIGBV]) -> DiscreteCirc:
+        other: DiscreteCirc = canon(other)
+        both_valid = (self._vexpr & other._vexpr).with_output(self.valid_id)
+        circ = (self.circ | other.circ) >> both_valid.aigbv
+        return from_aigbv(
+            circ=circ,
+            input_encodings=fn.merge(other.input_encodings,
+                                     self.input_encodings),
+            output_encodings=fn.merge(other.output_encodings,
+                                      self.output_encodings),
+            valid_id=self.valid_id
+        )
 
     def __lshift__(self, other: DiscreteCirc) -> DiscreteCirc:
         pass
@@ -73,10 +105,15 @@ class DiscreteCirc:
         pass
 
 
+def canon(circ: Union[BV.AIGBV, DiscreteCirc]):
+    if not isinstance(circ, DiscreteCirc):
+        circ = from_aigbv(circ)
+    return circ.rename_valid()
 
-def from_aigbv(circ: BV.AIGBV, 
-               input_encodings: Encodings=None,
-               output_encodings: Encodings=None,
+
+def from_aigbv(circ: BV.AIGBV,
+               input_encodings: Encodings = None,
+               output_encodings: Encodings = None,
                valid_id="##valid") -> DiscreteCirc:
     if input_encodings is None:
         input_encodings = {}
@@ -88,7 +125,7 @@ def from_aigbv(circ: BV.AIGBV,
     return DiscreteCirc(
         circ=circ,
         input_encodings=input_encodings,
-        output_encodings=output_encodings,        
+        output_encodings=output_encodings,
     )
 
 
--- tests/test_discrete.py
+++ tests/test_discrete.py
@@ -2,7 +2,7 @@ import pytest
 
 import aiger_bv as BV
 
-from aiger_discrete import DiscreteCirc, Encoding, from_aigbv
+from aiger_discrete import Encoding, from_aigbv
 
 
 def test_discrete_wrapper_smoke():
@@ -22,7 +22,7 @@ def test_discrete_wrapper_smoke():
         circ,
         input_encodings={'x': int_enc},
         output_encodings={'z': int_enc},
-    )        
+    )
     assert func2({'x': 2})[0] == {'z': 3}
 
     valid = (x <= 2).with_output('##valid') \
@@ -33,7 +33,40 @@ def test_discrete_wrapper_smoke():
         input_encodings={'x': int_enc},
         output_encodings={'z': int_enc},
     )
-    assert func3({'x': 2})[0] == {'z': 3}    
+    assert func3({'x': 2})[0] == {'z': 3}
 
     with pytest.raises(ValueError):
         func3({'x': 3})
+
+    assert func3.inputs == {'x'}
+    assert func3.outputs == {'z'}
+    assert func3.latches == set()
+    assert func3.latch2init == {}
+
+
+def test_rename_valid():
+    func = from_aigbv(BV.uatom(3, 'x').aigbv).rename_valid('foo')
+    assert 'foo' in func.circ.outputs
+    assert 'foo' == func.valid_id
+
+
+def test_parallel_composition():
+    x = BV.uatom(3, 'x')
+    circ1 = (x + 1).with_output('y').aigbv \
+        | (x < 5).with_output('##valid').aigbv
+    func1 = from_aigbv(circ1)
+
+    circ2 = x.with_output('z').aigbv \
+        | (x > 2).with_output('##valid').aigbv
+    func2 = from_aigbv(circ2)
+
+    func12 = func1 | func2
+    assert func12({'x': (1, 1, 0)})[0] == {
+        'y': (0, 0, 1), 'z': (1, 1, 0)
+    }
+
+    with pytest.raises(ValueError):
+        func12({'x': (0, 0, 0)})
+
+    with pytest.raises(ValueError):
+        func12({'x': (1, 1, 1)})
